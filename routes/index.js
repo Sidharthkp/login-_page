ae389ed13b915970512fa1505e4ee79b8dbb8cf8
Sidharthkp <Sidharthkannan20@gmail.com>
2022-10-10 02:22:57 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,20 @@ var router = express.Router();
 router.get('/', function (req, res, next) {
   let card = [
     {
-      name: Ubuntu
+      name: "Ubuntu",
+      image: "https://funnytrivias.com/wp-content/uploads/2021/08/ubuntuFunnyTriviaLinuxDistro-1.png"
     },
     {
-      name: Kali
+      name: "Kali",
+      image: "https://images.cults3d.com/iQgdEZYLHqJN_4xOs8-5gsd0ft8=/516x516/https://files.cults3d.com/uploaders/13889723/illustration-file/88f914f9-4ec0-4d0c-8ebb-5edc51f4b3cd/kali_linux.jpg"
     },
     {
-      name: Manjaro
+      name: "Manjaro",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Manjaro-logo.svg/2048px-Manjaro-logo.svg.png"
     },
     {
-      name: Mint
+      name: "Mint",
+      image: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Linux_Mint_logo_without_wordmark.svg/1200px-Linux_Mint_logo_without_wordmark.svg.png"
     }
   ]
   res.render('index', { card });
